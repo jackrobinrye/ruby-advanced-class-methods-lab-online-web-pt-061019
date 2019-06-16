c8e7e09b28488262c2291775e1050ca4f96390c6
Jack Atherton <jackrrye@gmail.com>
2019-06-16 18:15:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -64,7 +64,7 @@ class Song
     filename_array = filename.split(" - ")
     filename_name_array = filename_array[1].split(".")
     filename_song = Song.create_by_name(filename_name_array[0])
-    filename_song.artist = filename_array[0]
+    filename_song.artist_name = filename_array[0]
     binding.pry
   end
 
